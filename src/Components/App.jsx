2454ce8cd60018de1914893bdfcd754cc53e5ecc
Jean-Carlos <jeanc.aldea1@gmail.com>
2020-02-04 16:36:28 -0300

    Creacion de componente Card y route Se crea el componente card y se le va asignando rutas  al patch

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,6 +4,8 @@ import Banner from './Molecules/Banner';
 import Lista from '../Arrays/Lista.json';
 import Cards from './Molecules/Cards';
 import Form from './Organism/Form';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Card from './Molecules/Card';
 
 // En esta pieza de codigo,modifico el componente App a clase y agrego un state
 // dentro del state creo un array lista y le asigno los Valores de mi Lista.json
@@ -15,26 +17,57 @@ class App extends Component {
   // =================================
 
   render() {
-    return <>
-    <Banner></Banner>
+    return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={ () => (
+          <>
+          <Banner></Banner>
 
-    <div className="ed-grid m-grid-3">
-      {
-        // aca declaro this.state y le agrego la funcion map la cual recorre el array
-        // agrego el componente cards y le asigno e.valor segun el atributo
-        this.state.lista.map( e => 
-          <Cards 
-          id={e.id} 
-          title={e.title} 
-          description={e.description}
-          image={e.image}
-          price={e.price}
-          key={e.id}  
-          />)
-      }
-    </div>
-    <Form/>
-    </>
+          <div className="ed-grid m-grid-3">
+            {
+              // aca declaro this.state y le agrego la funcion map la cual recorre el array
+              // agrego el componente cards y le asigno e.valor segun el atributo
+              this.state.lista.map( e => 
+                <Cards 
+                id={e.id} 
+                title={e.title} 
+                description={e.description}
+                image={e.image}
+                price={e.price}
+                key={e.id}  
+                />)
+            }
+          </div>
+          <Form/>
+          </>
+        )}></Route>
+        <Route path="/cards/:id" component={Card}/>
+        <Route path="/cards" component={ () => 
+          <div className="ed-grid m-grid-3">
+          {
+            // aca declaro this.state y le agrego la funcion map la cual recorre el array
+            // agrego el componente cards y le asigno e.valor segun el atributo
+            this.state.lista.map( e => 
+              <Cards 
+              id={e.id} 
+              title={e.title} 
+              description={e.description}
+              image={e.image}
+              price={e.price}
+              key={e.id}  
+              />)
+          }
+        </div>}/>
+        <Route path="/form" component={Form}/>
+        <Route component={ () => <div>
+              <h1>Error 404</h1>
+              <span>Página no encontrada</span>
+            </div>
+        }/>
+
+      </Switch>
+    </Router> )
   }
 }
 
---
+++ src/Components/Molecules/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Lista from '../../Arrays/Lista';
+
+//Match me permite poder acceder a parametros del patch de la ruta}
+//Se crea una constante con el array importado, con el Find se busca alguna coincidencia y si la hay
+//retornará segun la informacion que corresponde
+const Card = ({ match }) => {
+    const product = Lista.find( p => p.id === parseInt(match.params.id))
+    return (
+        <div className="ed-grid m-grid-3">
+                {
+                    product ? (
+                        <>
+                        <h1 className="m-cols-3"> {product.title} </h1>
+                        <img className="m-cols-2" src={product.image} alt={ product.title }/>
+                        <p className="m-cols-1"> {product.description} </p>
+                        </>
+                    )
+                    :
+                    (
+                        <h1>This product not exits</h1>
+                    )
+                }
+            </div>
+        )
+    }
+export default Card;
