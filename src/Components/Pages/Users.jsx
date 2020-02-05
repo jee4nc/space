05ce55ffd8ad7443af8de9e3f3f445f16b5c31b8
Jean-Carlos <jeanc.aldea1@gmail.com>
2020-02-05 19:05:18 -0300

    Axios para Api Yarn add axios para poder ocupar axios como libreria, para poder consumir api de manera mas facil

--- src/Components/Pages/Users.jsx
+++ src/Components/Pages/Users.jsx
@@ -1,5 +1,6 @@
 import React,{Component} from 'react'
 import UserCard from '../Molecules/UserCard'
+import Axios from 'axios'
 
 class Users extends Component {
 
@@ -11,25 +12,22 @@ class Users extends Component {
             users : []
         }
     }
-    // ESTO ME PERMITE QUE CUANDO SE MONTE EL COMPONENTE HAGA LA SIGUIENTE ACCION 
-    // YA QUE JS ES ASINCRONO
-    componentDidMount() {
-        // SE TIENE QUE DAR FETCH A URL Y TAMBIEN UN OBJETO METHOD CON EL METODO EN ESTE CASO GET
-        fetch('https://jsonplaceholder.typicode.com/users',{method: 'get'})
-        .then(response => response.json()) //SE TIENE QUE HACER ESTE CALLBACK
-        .then(response2 => {            //Y DESPUES ESTE DE ACA 
-            this.setState({     //EN ESTE SE TIENE QUE CAMBIAR EL ESTADO DE ARRIBA, users Con response2
-                users: response2
-            })
-        } )
-    }
 
+    //YARN ADD AXIOS e importarlo para facilitar el consumir una api
+    componentDidMount() {
+        Axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+            this.setState ( {
+                users : response.data //axios devuelve un objeto, y data es la "data" del objeto
+            })
+        })
+    }
     render() {
         const {users} = this.state      //SE CREA CONSTANTE CON USERS DE STATE Y EL VALOR DE THIS.STATE
         return (
             <div className="ed-grid">
                 <h1>Usuarios</h1>
-                <div>
+                <div className="ed-grid s-grid-2 m-grid-3 l-grid-4">
                     {
                         users.map ( u =>        //Y SE MAPEA COMO YA SABEMOS
                             <UserCard
@@ -44,5 +42,21 @@ class Users extends Component {
         )
     }
 }
+    //Otra forma de hacerlo con fetch
+    //====================================================================================
+    // ESTO ME PERMITE QUE CUANDO SE MONTE EL COMPONENTE HAGA LA SIGUIENTE ACCION 
+    // YA QUE JS ES ASINCRONO
+    // componentDidMount() {
+    //     // SE TIENE QUE DAR FETCH A URL Y TAMBIEN UN OBJETO METHOD CON EL METODO EN ESTE CASO GET
+    //     fetch('https://jsonplaceholder.typicode.com/users',{method: 'get'})
+    //     .then(response => response.json()) //Primer then obtengo la respuesta y ejecuto cadena json
+    //     .then(response2 => {            //una ves creado el json recien modifico estado
+    //         this.setState({     //EN ESTE SE TIENE QUE CAMBIAR EL ESTADO DE ARRIBA, users Con response2
+    //             users: response2
+    //         })
+    //     } )
+    // }
+    //=================================================================================================
+    
 
 export default Users
